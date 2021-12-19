51a8daeb30a2cd886e93e7573d9c20c0c7a49bd9
Paul Fentress <fentresspaul@berkeley.edu>
2021-12-19 01:03:18 -0800

    initial commit

---
+++ Helper.py
@@ -0,0 +1,37 @@
+# Installing Dependincies
+import matplotlib.image as img
+import matplotlib.pyplot as plt
+import cv2
+import pandas as pd
+import numpy as np
+
+# Global Variable for Image Size
+IMG_SIZE = 28
+
+def test():
+    print("Jar Jar")
+
+def image_reducer(path, size=IMG_SIZE):
+    """
+    Function: image_reducer(path, size=IMG_SIZE)
+    Inputs:
+    - path
+      path is a file path from anywhere,
+
+    - size
+      dimension of square image to be reduced into
+
+
+    Outputs:
+    reduced image of data type np.array
+    """
+    # Reading in the image from path
+    image = img.imread(path)
+
+    # Reducing from 3d to 2d shape
+    image = image[:,:, 0]
+
+    # Resizeing Image
+    reduced_img = cv2.resize(image, (size, size))
+
+    return reduced_img
---
+++ Model.py
@@ -0,0 +1,31 @@
+import Helper
+import pickle
+IMG_SIZE=28
+
+with open("Smiley_Data/CNN_Model.pkl", "rb") as pickle_file:
+  CNN_model = pickle.load(pickle_file)
+
+"""
+This Function takes in a path of an image, and resizes
+it to the specified IMG size
+
+Then reshapes the image into a convolutional input value of
+(1,28,28,1)
+
+Then calls the model.predict function on this input
+
+The output is dictionary with two keys: Happy / Sad
+and there respective probabilities (confidence) for
+the prediction
+
+"""
+
+def make_prediction(path, IMG_SIZE=IMG_SIZE):
+  data = Helper.image_reducer(path).reshape(1, IMG_SIZE, IMG_SIZE, 1)
+  prediction = CNN_model.predict(data)[0]
+  return_dict = {"Sad": prediction[0], "Happy": prediction[1] }
+  return return_dict
+
+path = "Test_Image.png"
+
+print(make_prediction(path))
